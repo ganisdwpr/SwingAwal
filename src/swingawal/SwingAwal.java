292d12e606699656c7a8357322370b5eee01ea6a
Lab Informatika <Lab Informatika@LabSisdig-11>
2020-03-03 08:46:58 +0700

    JFRAME

---
+++ src/swingawal/SwingAwal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingawal;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class SwingAwal {
+
+  public static void main(String [] args) {
+     JFrame frame = new JFrame("Ini Judulnya");
+     frame.setSize(300,200);
+     frame.setVisible(true); 
+     frame.setLocation(450,300);
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
+
